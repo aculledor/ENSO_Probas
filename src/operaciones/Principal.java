a34e1b5ed8e96e59d2230d0494a5eafa9766c3a5
Neverah <miniyos2@gmail.com>
2021-04-20 10:46:22 +0200

    UwU

--- src/operaciones/Principal.java
+++ src/operaciones/Principal.java
@@ -10,15 +10,7 @@ public class Principal {
 		System.out.println(ca.resta(7, 3));
 		ca.suma(5, 6);
 		ca.resta(7, 3);
-<<<<<<< HEAD
 		System.out.println("Ahora soy el último comentario! Muajajajajaj!!!");
-=======
-		System.out.println("UwU");
-		System.out.println("Prueba 2");
-		System.out.println("Soy yo");
-		System.out.println("Soy Acu2");
-		System.out.println("Soy Noelia");
->>>>>>> branch 'master' of https://github.com/aculledor/PrjGrp3E.git
 	}
 
 }
