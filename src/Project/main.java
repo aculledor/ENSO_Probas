8afe051af4391e20de821015860785cf74cd7ad2
Abraham Trashorras <abraham.cocodin@gmail.com>
2021-04-20 13:09:01 +0200

    Implemented main class

---
+++ src/Project/main.java
@@ -0,0 +1,16 @@
+package Project;
+
+import ModeladoDeDatos.*;
+import SubsAnalisisEstadisticos.*;
+import SubsGestionIncidencias.*;
+import SubsGestionOOTT.*;
+import SubsGestionProcesos.*;
+
+
+public class main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
