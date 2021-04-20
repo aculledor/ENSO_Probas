a5808960f6e491cc656415704a5bc6f930db55e2
Neverah <miniyos2@gmail.com>
2021-04-20 12:38:57 +0200

    Modelado de datos hecho

--- src/ModeladoDeDatos/OT.java
+++ src/ModeladoDeDatos/OT.java
@@ -1,5 +1,116 @@
 package ModeladoDeDatos;
 
+import java.util.ArrayList;
+
 public class OT {
 
+	// Atributos
+	private String ID;
+	private String descripcion;
+	private ArrayList<String> material;
+	private ArrayList<Float> presupuestos;
+	private Float coste;
+	private String responsable;
+	private ArrayList<String> personal;
+	private String fechaInicio;
+	private Integer duracion;
+	private String estado;
+	
+	// Constructor
+	public OT(String ID, String descripcion, ArrayList<String> material, ArrayList<Float> presupuestos, Float coste,
+			String responsable, ArrayList<String> personal, String fechaInicio, Integer duracion, String estado) {
+		super();
+		this.ID = ID;
+		this.descripcion = descripcion;
+		this.material = material;
+		this.presupuestos = presupuestos;
+		this.coste = coste;
+		this.responsable = responsable;
+		this.personal = personal;
+		this.fechaInicio = fechaInicio;
+		this.duracion = duracion;
+		this.estado = estado;
+	}
+
+	// Getters y setters
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public ArrayList<String> getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(ArrayList<String> material) {
+		this.material = material;
+	}
+
+	public ArrayList<Float> getPresupuestos() {
+		return presupuestos;
+	}
+
+	public void setPresupuestos(ArrayList<Float> presupuestos) {
+		this.presupuestos = presupuestos;
+	}
+
+	public Float getCoste() {
+		return coste;
+	}
+
+	public void setCoste(Float coste) {
+		this.coste = coste;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+
+	public ArrayList<String> getPersonal() {
+		return personal;
+	}
+
+	public void setPersonal(ArrayList<String> personal) {
+		this.personal = personal;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Integer getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
 }
--- src/ModeladoDeDatos/Proceso.java
+++ src/ModeladoDeDatos/Proceso.java
@@ -1,5 +1,86 @@
 package ModeladoDeDatos;
 
 public class Proceso {
+	
+	// Atributos
+	private String nombre;
+	private String descripcion;
+	private Float coste;
+	private Integer estimado;
+	private String estado;
+	private String responsable;
+	private String servicio;
+	
+	// Constructor
+	
+	public Proceso(String nombre, String descripcion, Float coste, Integer estimado, String estado, String responsable,
+			String servicio) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.coste = coste;
+		this.estimado = estimado;
+		this.estado = estado;
+		this.responsable = responsable;
+		this.servicio = servicio;
+	}
+	
+	// Getters y setters
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Float getCoste() {
+		return coste;
+	}
+
+	public void setCoste(Float coste) {
+		this.coste = coste;
+	}
+
+	public Integer getEstimado() {
+		return estimado;
+	}
+
+	public void setEstimado(Integer estimado) {
+		this.estimado = estimado;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+
+	public String getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(String servicio) {
+		this.servicio = servicio;
+	}
+	
 }
