cdcc6f416ef100db9429ebb04c98765a79291501
Abraham Trashorras <abraham.cocodin@gmail.com>
2021-04-20 12:32:02 +0200

    Hot fixes, Interface functions and default null implementations

--- src/SubsAnalisisEstadisticos/InterfazGestorEstadisticas.java
+++ src/SubsAnalisisEstadisticos/InterfazGestorEstadisticas.java
@@ -1,5 +1,14 @@
 package SubsAnalisisEstadisticos;
 
-public interface InterfazGestorEstadisticas {
+import java.util.HashMap;
 
+public interface InterfazGestorEstadisticas {
+	Integer totalIncidencias(String campoFiltro, String valorFiltro);
+	Integer totalProcesos(String campoFiltro, String valorFiltro);
+	Integer totalOOTT(String campoFiltro, String valorFiltro);
+	Float costesProcesos(String campoFiltro, String valorFiltro);
+	Float costesOOTT(String campoFiltro, String valorFiltro);
+	HashMap<String, Integer> distribuciónIncidencias(String campoFiltro, String valorFiltro);
+	HashMap<String, Integer> distribuciónProcesos(String campoFiltro, String valorFiltro);
+	HashMap<String, Integer> distribuciónOOTT(String campoFiltro, String valorFiltro);
 }
