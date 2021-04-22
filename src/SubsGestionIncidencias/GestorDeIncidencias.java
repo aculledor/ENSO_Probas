0a9cfb4dd43e53ae6d122a0c8d0d2d6a59dde23f
sualci <63094931+sualci@users.noreply.github.com>
2021-04-22 19:21:43 +0200

    implementation revised

--- src/SubsGestionIncidencias/GestorDeIncidencias.java
+++ src/SubsGestionIncidencias/GestorDeIncidencias.java
@@ -7,16 +7,19 @@ import ModeladoDeDatos.Incidencia;
 public class GestorDeIncidencias implements InterfazGestorIncidencias {
 
 	ArrayList<Incidencia> incidencias;
+	private Integer idCount;
 
 	// Constructors
 	public GestorDeIncidencias(ArrayList<Incidencia> incidencias) {
 		super();
 		this.incidencias = incidencias;
+		idCount = 0;
 	}
 
 	public GestorDeIncidencias() {
 		super();
 		this.incidencias = new ArrayList<>();
+		idCount = 0;
 	}
 
 	@Override
@@ -37,7 +40,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 							// Se añade a la lista "resultado" cada elemento que cumpla el filtro
 							if (i.getID().equals(valorFiltro))
 								resultado.add(i);
-						// Se devuelve el contador
+						// Se devuelve la lista
 						return resultado;
 					} else {
 						System.out.println("El valor del filtro tiene un valor o formato incorrecto");
@@ -56,7 +59,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 								// Se aáde a la lista el elemento que cumpla el filtro
 								if (i.getNombreCiudadano().equals(valorFiltro))
 									resultado.add(i);
-							// Se devuelve el contador
+							// Se devuelve la lista
 							return resultado;
 						}else {
 						// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
@@ -68,26 +71,55 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 					System.out.println("El valor del filtro no puede ser nulo si se ha indicado un campo de filtro");
 					return null;
 				case "DNI":
+					int sum=0, dniCount=0, mod;
+					Character mayus, minus;
+					String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
 					// Se comprueba que el valor del filtro no sea nulo
 					if (valorFiltro != null) {
 						// Se comprueba que se emplea el formato correcto
-						if (valorFiltro.length() == 9 && Character.isDigit(valorFiltro.charAt(0))
-								&& Character.isDigit(valorFiltro.charAt(1)) && Character.isDigit(valorFiltro.charAt(2))
-								&& Character.isDigit(valorFiltro.charAt(3)) && Character.isDigit(valorFiltro.charAt(4))
-								&& Character.isDigit(valorFiltro.charAt(5)) && Character.isDigit(valorFiltro.charAt(6))
-								&& Character.isDigit(valorFiltro.charAt(7)) && Character.isLetter(valorFiltro.charAt(8))) {
-							// Se recorre la lista
-							for (Incidencia i : incidencias)
-								// Se añade a la lista cada elemento que cumpla el filtro
-								if (i.getDNI().equals(valorFiltro))
-									resultado.add(i);
-							// Se devuelve el contador
-							return resultado;
+						if (valorFiltro.length() == 9) {
+							// contamos cuentos numeros hay y los sumamos
+							for (int i = 0; i < valorFiltro.length(); i++) {
+								if (Character.isDigit(valorFiltro.charAt(i))) {
+									dniCount++;
+									sum += Character.getNumericValue(valorFiltro.charAt(i));
+								}
+							}
+							// si el dni tiene 8 números
+							if (dniCount == 8) {
+								// comprobamos que el caracter final es una letra
+								if (Character.isAlphabetic(valorFiltro.charAt(valorFiltro.length() - 1))) {
+									// comprobamos que la letra es la que le corresponde
+									mod = sum % 23 - 1;
+									// obtenemos la letra esperada
+									mayus = letras.charAt(mod);
+									// pasamos la letra esperada a minuscula
+									minus = letras.toLowerCase().charAt(mod);
+									// si la letra introducida no es la esperdada en mayuscula
+									if ( mayus.equals(valorFiltro.charAt(valorFiltro.length() - 1)) || minus.equals(valorFiltro.charAt(valorFiltro.length() - 1))) {
+										// Se recorre la lista
+										for (Incidencia i : incidencias)
+											// Se añade a la lista cada elemento que cumpla el filtro
+											if (i.getDNI().equals(valorFiltro))
+												resultado.add(i);
+										// Se devuelve la lista
+										return resultado;
+									}else {
+										System.out.println("El dni tiene un formato incorrecto. La letar no coincide.");
+										return null;
+									}
+								}else {
+									System.out.println("El dni tiene un formato incorrecto. Debe contener 8 cifras + 1 letra.");
+									return null;
+								}
+							} else {
+								System.out.println("El dni tiene un formato incorrecto. Debe contener 8 cifras + 1 letra.");
+								return null;
+							}
+						}else {
+							System.out.println("El dni tiene un formato incorrecto. Debe ser una cadena de 9 caracetres");
+							return null;
 						}
-						// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
-						// usuario y se devuelve un null
-						System.out.println("El valor del filtro tiene un valor o formato incorrecto");
-						return null;
 					}
 					// Si el valor del filtro es nulo, se informa al usuario y se devuelve un null
 					System.out.println("El valor del filtro no puede ser nulo si se ha indicado un campo de filtro");
@@ -109,7 +141,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 								// Se añade a la lista cada elemento que cumpla el filtro
 								if (i.getTelefono().equals(valorFiltro))
 									resultado.add(i);
-							// Se devuelve el contador
+							// Se devuelve la lista
 							return resultado;
 						}
 						// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
@@ -230,44 +262,44 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 				for (int i = 0; i < DNI.length(); i++) {
 					if (Character.isDigit(DNI.charAt(i))) {
 						dniCount++;
-						sum += Integer.valueOf(DNI.charAt(i));
+						sum += Character.getNumericValue(DNI.charAt(i));
 					}
 				}
 				// si el dni tiene 8 números
 				if (dniCount == 8) {
 					// comprobamos que el caracter final es una letra
 					if (!Character.isAlphabetic(DNI.charAt(DNI.length() - 1))) {
-						System.out.println("el dni debe contener 8 núemros seguidos de una letra");
+						System.out.println("el dni debe contener 8 números seguidos de una letra");
 						return null;
 					}
 					// comprobamos que la letra es la que le corresponde
-					mod = sum % 23;
+					mod = sum % 23 - 1;
 					// obtenemos la letra esperada
 					mayus = letras.charAt(mod);
 					// pasamos la letra esperada a minuscula
 					minus = letras.toLowerCase().charAt(mod);
 					// si la letra introducida no es la esperdada en mayuscula
-					if (!mayus.equals(DNI.charAt(DNI.length() - 1))) {
-						System.out.println("El dni no tiene un formato válido");
-						return null;
-					}
-					// si la letra introducida no es la esperada en minuscula
-					else if (!minus.equals(DNI.charAt(DNI.length() - 1))) {
-						System.out.println("El dni no tiene un formato válido");
-						return null;
-					} else {
-						System.out.println("El dni debe contener 8 cifras");
-						return null;
+					if (!(mayus.equals(DNI.charAt(DNI.length() - 1)))) {
+						// si la letra introducida no es la esperada en minuscula
+						if (!(minus.equals(DNI.charAt(DNI.length() - 1)))) {
+							//informamos al usuario y enviamos null
+							System.out.println("El dni no tiene un formato válido. Letra no coincide.");
+							return null;
+						}
 					}
 				} else {
-					System.out.println("El dni tiene un formato incorrecto");
+					System.out.println("El dni tiene un formato incorrecto. Debe contener 8 cifras + 1 letra.");
 					return null;
 				}
 			} else {
-				System.out.println("El dni es null");
+				System.out.println("El dni tiene un formato incorrecto. Debe contener 9 carácteres.");
 				return null;
 			}
+		}else {
+			System.out.println("El dni es null");
+			return null;
 		}
+		
 		// compruebo validez del nombre
 		if (nombre == null) {
 			System.out.println("El nombre es null");
@@ -308,7 +340,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 		// compruebo la descripcion
 		if (descripcion != null) {
 			if (descripcion.length() > 0) {
-				if (descripcion.length() <= 20) {
+				if (descripcion.length() <= 200) {
 					// compruebo la localizacion
 					if (localizacion != null) {
 						if (localizacion.length() > 0) {
@@ -319,11 +351,9 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 										if (tipo.length() <= 20) {
 											// si todos los campos son válidos
 											// se instancia la Incidencia
-											id = ((Integer) incidencias.size()).toString(); // se le asigna como id, el
-																							// numero de incidencias que
-																							// hay en el
-																							// array
-											inc = new Incidencia(id, nombre, DNI, tlf, descripcion, localizacion, tipo);
+											inc = new Incidencia(idCount.toString(), nombre, DNI, tlf, descripcion, localizacion, tipo);
+											//se incrementa una unidad, de forma que el id se genera secuencialmente
+											idCount++;
 											// se guarda la incidencia en el array
 											incidencias.add(inc);
 											// si todos los campos son válidos se devuelve la instancia de la incidencia
@@ -362,7 +392,8 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 						// si el campo a cambiar es null
 						if (campo == null) {
 							// aviso al usuario
-							System.out.println("El campo especificado es null, no se peude realizar esta acción");
+							System.out.println("El campo especificado es null, no se puede realizar esta acción");
+							return;
 						} else {
 							//dependiendo del campo introducido
 							switch (campo) {
@@ -373,15 +404,14 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 									if (valor.length() > 0 && valor.length() <= 200) {
 										//Se modifica la incidencia
 										inc.setDescripcion(valor);
+										return;
 									}
-									// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
-									// usuario y se devuelve un null
+									// Si el valor del filtro tiene un valor o formato incorrecto, se informa al usuario
 									System.out.println("El valor del filtro tiene un valor o formato incorrecto");
-
 								}
 								// Si el valor del filtro es nulo, se informa al usuario y se devuelve un null
 								System.out.println("El valor del filtro no puede ser nulo si se ha indicado un campo de filtro");
-
+								break;
 							case "localizacion":
 								// Se comprueba que el valor del filtro no sea nulo
 								if (valor != null) {
@@ -390,6 +420,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 										// Se recorre la lista
 										//Se modifica la incidencia
 										inc.setLocalizacion(valor);
+										return;
 									}
 									// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
 									// usuario y se devuelve un null
@@ -398,7 +429,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 								}
 								// Si el valor del filtro es nulo, se informa al usuario y se devuelve un null
 								System.out.println("El valor del filtro no puede ser nulo si se ha indicado un campo de filtro");
-
+								break;
 							case "tipo":
 								// Se comprueba que el valor del filtro no sea nulo
 								if (valor != null) {
@@ -406,6 +437,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 									if (valor.length() > 0 && valor.length() <= 20) {
 										//Se modifica la incidencia
 										inc.setTipo(valor);
+										return;
 									}
 									// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
 									// usuario y se devuelve un null
@@ -413,6 +445,7 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 								}
 								// Si el valor del filtro es nulo, se informa al usuario y se devuelve un null
 								System.out.println("El valor del filtro no puede ser nulo si se ha indicado un campo de filtro");
+								break;
 							case"id":
 							case "nombreCiudadano":
 							case "dni":
@@ -425,15 +458,19 @@ public class GestorDeIncidencias implements InterfazGestorIncidencias {
 							}
 						}
 						
-						//finalizamos la funcion
-						return;
 					}
 				}
+				//si termina el bucle for sin encuontrar la incidencia e avisa al usuario
+				System.out.println("No existe ninguna incidencia con ese ID");
+				return;
 				
 			}
-			// Si el valor del filtro tiene un valor o formato incorrecto, se informa al
-			// usuario y se devuelve un null
+			// Si el valor del filtro tiene un valor o formato incorrecto, se informa al usuario
 			System.out.println("El valor del ID tiene un valor o formato incorrecto");
+			return;
+		}else {
+			//si el id es nulll se avisa al usuario
+			System.out.println("El ID es null");
 		}
 
 	}
