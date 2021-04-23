609cb479a6e39fd11549819133690792c5ce1df1
Abraham Trashorras <abraham.cocodin@gmail.com>
2021-04-23 19:23:59 +0200

    Implemented toString, equals and hashcode in the data models

--- src/ModeladoDeDatos/Incidencia.java
+++ src/ModeladoDeDatos/Incidencia.java
@@ -100,6 +100,35 @@ public class Incidencia {
 	public String getFechaCreacion() {
 		return fechaCreacion;
 	}
+	@Override
+	public String toString() {
+		return "Incidencia [ID=" + ID + ", nombreCiudadano=" + nombreCiudadano + ", DNI=" + DNI + ", telefono="
+				+ telefono + ", descripcion=" + descripcion + ", localizacion=" + localizacion + ", tipo=" + tipo
+				+ ", proceso=" + proceso + ", fechaCreacion=" + fechaCreacion + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Incidencia other = (Incidencia) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
 	
 	
 }
